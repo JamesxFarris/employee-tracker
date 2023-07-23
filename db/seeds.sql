0fa8f6af9bdbfe539ab9bd5f648cf81cf529fb6e
James Farris <jafarris.exe@gmail.com>
2023-07-23 00:51:00 -0400

    Added seeds and fixed bug in db

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,6 +9,15 @@ VALUES
 INSERT INTO
     role (title, salary, department_id)
 VALUES
-    ("Sales Lead, 100000, 1"),
-    ("Software Engineer, 250000, 2"),
-    ("Accountant, 85000, 3") ("Lawyer, 200000, 4")
+    ("Sales Lead", 100000, 1),
+    ("Software Engineer", 250000, 2),
+    ("Accountant", 85000, 3),
+    ("Lawyer", 200000, 4);
+
+INSERT INTO
+    employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Belon", "Tusk", 2, NULL),
+    ("Boris", "Johnson", 4, 1),
+    ("Donald", "Trump", 1, 1),
+    ("Stacking", "Stanley", 3, 1);
